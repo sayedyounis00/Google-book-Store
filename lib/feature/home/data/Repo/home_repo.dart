cfa349dabbb32b00afbb86a2f8db2ea25881ceec
sayed_younis <elsayedseif17@gmail.com>
2025-02-16 04:22:14 +0200

    create home repo

---
+++ lib/feature/home/data/Repo/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/feature/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  //here we add return type and agument 
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBook();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
+}
