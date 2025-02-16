260559e4294836c2dedd8966f8e0619fbd4d13cc
sayed_younis <elsayedseif17@gmail.com>
2025-02-17 00:31:16 +0200

    create server failure from dio error

--- lib/feature/home/data/Repo/home_repo_impl.dart
+++ lib/feature/home/data/Repo/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/core/services/api_services.dart';
 import 'package:bookly/feature/home/data/Repo/home_repo.dart';
 import 'package:bookly/feature/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiServices apiServices;
@@ -20,7 +21,10 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 
