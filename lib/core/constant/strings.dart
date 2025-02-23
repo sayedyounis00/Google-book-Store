2c694edadd0c2f16cf33389d8b3e3280b012ee60
sayed_younis <elsayedseif17@gmail.com>
2025-02-23 06:42:46 +0200

    addign cubit and its states

--- lib/core/constant/strings.dart
+++ lib/core/constant/strings.dart
@@ -1,3 +1,7 @@
 abstract class Strings {
-  static const  String kBaseUrl = "https://www.googleapis.com/books/v1/";
+  static const String kBaseUrl = "https://www.googleapis.com/books/v1/";
+  static const String endPointforNewest =
+      "Filtering=free-ebooks&q=subject:programming&Sorting=newest";
+  static const String endPointforFeatured =
+      "Filtering=free-ebooks&q=subject:programming";
 }
